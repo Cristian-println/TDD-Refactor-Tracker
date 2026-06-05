6794d43b9be5c340b4c6cb5e24d8039d7d12db1f
VelizHuanaco <202206424@est.umss.edu>
2026-06-05 13:26:31 -0400

    Fase roja completada del test (US-1)

--- test/com/herramienta/us01/CapturadorEntornoTest.java
+++ test/com/herramienta/us01/CapturadorEntornoTest.java
@@ -1,5 +1,20 @@
 package com.herramienta.us01;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CapturadorEntornoTest {
-
+	
+	@Test
+	public void deberiaQuitarEspaciosDelTexto() {
+		// Preparar (Arrange)
+		CapturadorEntorno capturador = new CapturadorEntorno();
+		String textoBruto = "  Texto_Sucio  " ;
+		
+		// Actuar (Act)
+		String textoLimpio = capturador.obtenerClaseLimpiada(textoBruto);
+		
+		// Comprobar (Assert)
+		assertEquals("Texto_Sucio", textoLimpio);
+	}
+	
 }
